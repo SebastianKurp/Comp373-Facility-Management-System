e15146e38c60ba1d55f4fa29c1bd79a7bb17fb69
Sebastian Kurpiel <sebbykurp@gmail.com>
2019-02-11 20:07:10 -0600

    Commiting files I forgot to commit

--- src/main/java/FMS/Link.java
+++ src/main/java/FMS/Link.java
@@ -1,4 +1,20 @@
 package FMS;
 
 public class Link {
+
+    private final String url;
+    private final String description;
+
+    public Link(String url, String description) {
+        this.url = url;
+        this.description = description;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
--- src/main/java/FMS/Query.java
+++ src/main/java/FMS/Query.java
@@ -1,4 +1,18 @@
 package FMS;
 
-public class Query {
+import com.coxautodev.graphql.tools.GraphQLRootResolver;
+
+import java.util.List;
+
+public class Query implements GraphQLRootResolver {
+
+    private final LinkRepository linkRepository;
+
+    public Query(LinkRepository linkRepository) {
+        this.linkRepository = linkRepository;
+    }
+
+    public List<Link> allLinks() {
+        return linkRepository.getAllLinks();
+    }
 }
