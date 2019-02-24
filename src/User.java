d09a33d9df699b35d21e4c3551de275dcfb0a09e
Sebastian Kurpiel <sebbykurp@gmail.com>
2019-02-23 23:39:37 -0600

    Added the setter for the user

--- src/User.java
+++ src/User.java
@@ -1,9 +1,17 @@
 public class User {
     String id;
     int room;
-    String userNotes[];
+    // String userNotes[];
     String phoneNumber;
     String email;
     String name;
+
+    public User(int id, int room, String phoneNumber, String email, String name) {
+        this.id = id;
+        this.room = room;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.name = name;
+    }
 
 }
