9f1168d439f034a917921849c83efd5f8545fbf6
Sebastian Kurpiel <sebbykurp@gmail.com>
2019-04-21 17:11:03 -0500

    Getter methods for user registry

--- src/main/client/buildingAdmin/BuildingAdminInterface.java
+++ src/main/client/buildingAdmin/BuildingAdminInterface.java
@@ -1,12 +1,22 @@
 package main.client.buildingAdmin;
 
+import main.model.user.UserRegistry;
+
 public interface BuildingAdminInterface {
     //constructor method for most models
-    public void createNewBuilding();
-    public void createNewFloor();
-    public void createNewRoom();
-    public void createNewMailRoom();
-    public void createNewMaintenanceOffice();
-    public void createNewUser();
-    public void createNewStaff();
+    void createNewBuilding();
+    void createNewFloor();
+    void createNewRoom();
+    void createNewMailRoom();
+    void createNewMaintenanceOffice();
+    void createNewUser();
+    void createNewStaff();
+
+    //getters for users
+    void findUserByName(UserRegistry userRegistry,String name);
+    void findUserById(UserRegistry userRegistry,int id);
+    void findUserByRoom(UserRegistry userRegistry,int roomNumber);
+    void findUserByEmail(UserRegistry userRegistry,String email);
+    void indUserByPhoneNumber(UserRegistry userRegistry, String phonenumber);
+
 }
--- src/main/model/BuildingAdmin/BuildingAdmin.java
+++ src/main/model/BuildingAdmin/BuildingAdmin.java
@@ -7,9 +7,11 @@ import main.model.facility.MailRoom;
 import main.model.facility.Room;
 import main.model.user.User;
 import main.model.staff.Staff;
+import main.model.user.UserRegistry;
 
 public class BuildingAdmin {
 
+    //constructors for all models
     public Building createNewBuilding() {
         Building b = new Building();
         return b;
@@ -43,7 +45,33 @@ public class BuildingAdmin {
     public Staff createNewStaff(){
         Staff s = new Staff();
         return s;
-
     }
+
+    //methods for userRegistry
+    public UserRegistry findUserByName(UserRegistry userRegistry,String name){
+        User u = userRegistry.findUserByName(name);
+        return u;
+    }
+
+    public UserRegistry findUserById(UserRegistry userRegistry,int id){
+        User u = userRegistry.findUserById(id);
+        return u;
+    }
+
+    public UserRegistry findUserByRoom(UserRegistry userRegistry,int roomNumber){
+        User u = userRegistry.findUserByRoom(roomNumber);
+        return u;
+    }
+
+    public UserRegistry findUserByEmail(UserRegistry userRegistry,String email){
+        User u = userRegistry.findUserByEmail(email);
+        return u;
+    }
+
+    public UserRegistry findUserByPhoneNumber(UserRegistry userRegistry, String phonenumber){
+        User u = userRegistry.findUserByPhoneNumber(phonenumber);
+        return u;
+    }
+
 }
 
